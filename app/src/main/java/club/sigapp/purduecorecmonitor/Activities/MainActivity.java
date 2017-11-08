ff8ac2c3124de9acaf2cb9173e560dd65720660c
jeff <hughesjeffrey33@gmail.com>
2017-11-07 19:07:31 -0500

    Initial Commit

---
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Activities/MainActivity.java
@@ -0,0 +1,23 @@
+package club.sigapp.purduecorecmonitor.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import club.sigapp.purduecorecmonitor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}

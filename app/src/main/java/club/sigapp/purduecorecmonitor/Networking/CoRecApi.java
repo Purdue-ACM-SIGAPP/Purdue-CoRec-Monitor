fb5365f8e043498aea7654431711c11a88194ff7
vieck <vieck@purdue.edu>
2017-11-09 16:41:58 -0500

    Added Corec apis

--- app/src/main/java/club/sigapp/purduecorecmonitor/Networking/CoRecApi.java
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Networking/CoRecApi.java
@@ -1,4 +1,31 @@
 package club.sigapp.purduecorecmonitor.Networking;
 
+import java.util.List;
+
+import club.sigapp.purduecorecmonitor.Models.LastUpdatedModel;
+import club.sigapp.purduecorecmonitor.Models.LocationsModel;
+import club.sigapp.purduecorecmonitor.Models.MonthlyTrendsModel;
+import club.sigapp.purduecorecmonitor.Models.WeeklyTrendsModel;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface CoRecApi {
+    @GET("currentactivity")
+    Call<List<LocationsModel>> getAllLocations();
+
+    @GET("location/{location}")
+    Call<List<LocationsModel>> getLocationDetails(@Path("location") String location);
+
+    @GET("monthlytrends/{location}")
+    Call<List<MonthlyTrendsModel>> getLocationMonthlyTrend(@Path("location") String location);
+
+    @GET("weeklytrends/{location}")
+    Call<List<WeeklyTrendsModel>> getLocationWeeklyTrend(@Path("location") String location);
+
+    @GET("lastupdatedtime/{location}")
+    Call<List<LastUpdatedModel>> getLocationLastUpdatedTime(@Path("location") String location);
+
+    @GET("lastupdatedtime")
+    Call<List<LastUpdatedModel>> getLastUpdatedTime();
 }

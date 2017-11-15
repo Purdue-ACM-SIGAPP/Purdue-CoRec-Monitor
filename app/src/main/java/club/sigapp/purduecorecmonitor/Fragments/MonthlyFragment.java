d1e8eb3666de4f94d57afe49bfa8a5d40c48727e
vieck <vieck@purdue.edu>
2017-11-14 19:24:21 -0500

    Added Monthly Fragment

---
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Fragments/MonthlyFragment.java
@@ -0,0 +1,28 @@
+package club.sigapp.purduecorecmonitor.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import club.sigapp.purduecorecmonitor.R;
+
+public class MonthlyFragment extends Fragment {
+
+
+    public MonthlyFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_monthly, container, false);
+    }
+
+}

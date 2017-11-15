01d347d989bef1af970da3287f8819fa078c9229
epituch <32016786+epituch@users.noreply.github.com>
2017-11-14 19:45:19 -0500

    Added removeFavorites and getFavorites

--- app/src/main/java/club/sigapp/purduecorecmonitor/Utils/Favorites.java
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Utils/Favorites.java
@@ -9,24 +9,38 @@ import java.util.Set;
 public class Favorites {
     public static void addFavorite(Context c, String newFavorite) {
         SharedPreferences shared = SharedPrefsHelper.getSharedPrefs(c);
-        if (shared.contains("favorite")) {
-            Set<String> favorites = shared.getStringSet("favorite", null);
+        if (shared.contains("favorites")) {
+            Set<String> favorites = shared.getStringSet("favorites", null);
             if (!favorites.contains(newFavorite)) {
                 favorites.add(newFavorite);
-                shared.edit().remove("favorite").apply();
-                shared.edit().putStringSet("favorite", favorites).apply();
+                shared.edit().remove("favorites").apply();
+                shared.edit().putStringSet("favorites", favorites).apply();
             }
         } else {
             Set<String> newSet = new HashSet<>();
             newSet.add(newFavorite);
-            shared.edit().putStringSet("favorite", newSet).apply();
+            shared.edit().putStringSet("favorites", newSet).apply();
         }
-
     }
 
-    public static void removeFavorite(Context c, String newFavorite) {
-
-
+    public static void removeFavorite(Context c, String toRemoveFavorite) {
+        SharedPreferences shared = SharedPrefsHelper.getSharedPrefs(c);
+        if (shared.contains("favorites")) {
+            Set<String> favorites = shared.getStringSet("favorites", null);
+            if (favorites.contains(toRemoveFavorite)) {
+                favorites.remove(toRemoveFavorite);
+                shared.edit().remove("favorites").apply();
+                shared.edit().putStringSet("favorites", favorites).apply();
+            }
+        }
     }
 
+    public static Set<String> getFavorites(Context c) {
+        SharedPreferences shared = SharedPrefsHelper.getSharedPrefs(c);
+        if (shared.contains("favorites")) {
+            return shared.getStringSet("favorites", null);
+        } else {
+            return null;
+        }
+    }
 }

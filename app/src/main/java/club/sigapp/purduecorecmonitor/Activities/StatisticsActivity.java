7efa320452d24639df42ead8d395717db6b49de2
Chris Rosenblatt <rosench13@gmail.com>
2017-11-14 20:02:47 -0500

    created LineChart object

--- app/src/main/java/club/sigapp/purduecorecmonitor/Activities/StatisticsActivity.java
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Activities/StatisticsActivity.java
@@ -3,6 +3,11 @@ package club.sigapp.purduecorecmonitor.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+
 import club.sigapp.purduecorecmonitor.R;
 
 public class StatisticsActivity extends AppCompatActivity {
@@ -12,6 +17,13 @@ public class StatisticsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
 
+        LineChart chart = (LineChart) findViewById(R.id.chart);
 
+        double[] dataObjects = new double[1];
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        for(double data: dataObjects) {
+            //entries.add(new Entry(data.getValueX(), data.getValueY()));
+        }
     }
 }

a3c1f3582345c9cc9318128646bfc97a9907a7ef
Josh Kassman <jkassman2@gmail.com>
2017-11-28 19:01:39 -0500

    Added bar graph

--- app/src/main/java/club/sigapp/purduecorecmonitor/Fragments/WeeklyFragment.java
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Fragments/WeeklyFragment.java
@@ -7,10 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import club.sigapp.purduecorecmonitor.R;
+import club.sigapp.purduecorecmonitor.Utils.*;
 
 public class WeeklyFragment extends Fragment {
 
+    @BindView(R.id.bar_chart)
+    BarChart barChart;
 
     /* This is never used */
     public WeeklyFragment() {
@@ -23,7 +36,15 @@ public class WeeklyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weekly, container, false);
+        View view =  inflater.inflate(R.layout.fragment_weekly, container, false);
+        ButterKnife.bind(view);
+
+        List<BarEntry> barEntries = new ArrayList<>();
+        for(int i = 0; i < 7; i++) {
+            barEntries.add(new BarEntry(i,i));
+        }
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Label");
+        return view;
     }
 
 }
---
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Utils/Properties.java
@@ -0,0 +1,11 @@
+package club.sigapp.purduecorecmonitor.Utils;
+
+
+
+public class Properties {
+    private static String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+    public static String[] getDaysOfWeek() {
+        return daysOfWeek;
+    }
+}

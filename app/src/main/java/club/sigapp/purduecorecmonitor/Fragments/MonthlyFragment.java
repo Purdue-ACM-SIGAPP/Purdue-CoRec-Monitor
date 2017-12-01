95ae359d53ec3c9c488aba9793986da01809cb95
Vikas Tatineni <vtatinen@purdue.edu>
2017-11-30 19:56:10 -0500

    Stacked Line graph implementation with API data

--- app/src/main/java/club/sigapp/purduecorecmonitor/Fragments/MonthlyFragment.java
+++ app/src/main/java/club/sigapp/purduecorecmonitor/Fragments/MonthlyFragment.java
@@ -6,10 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.github.mikephil.charting.charts.LineChart;
-
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -17,18 +14,25 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import club.sigapp.purduecorecmonitor.Models.MonthlyTrendsModel;
 import club.sigapp.purduecorecmonitor.R;
+import club.sigapp.purduecorecmonitor.Networking.CoRecApi;
+import club.sigapp.purduecorecmonitor.Networking.CoRecApiHelper;
+import club.sigapp.purduecorecmonitor.Utils.MonthlyComparator;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MonthlyFragment extends Fragment {
 
-    @BindView(R.id.chart1)
-    LineChart mChart;
+    @BindView(R.id.stacked_Line_Chart)
+    LineChart stackedLineChart;
 
     public MonthlyFragment() {
         // Required empty public constructor
@@ -38,34 +42,53 @@ public class MonthlyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        super.onCreate(savedInstanceState);
         View view =  inflater.inflate(R.layout.fragment_monthly, container, false);
         ButterKnife.bind(this, view);
 
-        List<Entry> LineEntries = new ArrayList<>();
-        for(int i = 0; i < 7; i++) {
-            LineEntries.add(new Entry(i,10));
-        }
-
-
-        List<Entry> LineEntries2 = new ArrayList<>();
-
-        for(int i = 0; i < 7; i++) {
-            LineEntries2.add(new Entry(i,i));
-        }
-
-        List<ILineDataSet> IlineData = new ArrayList<>();
-
-        LineDataSet lineDataSet1 = new LineDataSet(LineEntries, "label");
-        LineDataSet lineDataSet2 = new LineDataSet(LineEntries2, "label");
-
-        IlineData.add(lineDataSet1);
-        IlineData.add(lineDataSet2);
-
-        LineData lineData = new LineData(IlineData);
-        mChart.setData(lineData);
-        mChart.invalidate();
+        initializeStackedLineChart();
         return view;
+    }
+
+    private void initializeStackedLineChart(){
+        final List<Entry> currentOccupancy = new ArrayList<>();
+        final List<Entry> maxOccupancy = new ArrayList<>();
+        final List<ILineDataSet> chartLines = new ArrayList<>();
+
+
+        CoRecApi api = CoRecApiHelper.getInstance();
+
+        api.getLocationMonthlyTrend("7071edb7-856e-4d05-8957-4001484f9aec").enqueue(new Callback<List<MonthlyTrendsModel>>() {
+            @Override
+            public void onResponse(Call<List<MonthlyTrendsModel>> call, Response<List<MonthlyTrendsModel>> response) {
+                if(response.code() == 200){
+                    List<MonthlyTrendsModel> monthlyTrendsModel = response.body();
+                    Collections.sort(monthlyTrendsModel, new MonthlyComparator());
+                    for(MonthlyTrendsModel data : monthlyTrendsModel){
+                        currentOccupancy.add(new Entry(data.Month,data.Headcount));
+                        maxOccupancy.add(new Entry(data.Month,data.Capacity));
+                    }
+
+                    LineDataSet maxCapacity = new LineDataSet(maxOccupancy, "Max Capacity");
+                    LineDataSet currentCapacity = new LineDataSet(currentOccupancy, "Average Capacity");
+                    chartLines.add(maxCapacity);
+                    chartLines.add(currentCapacity);
+
+                    LineData lineData = new LineData(chartLines);
+                    stackedLineChart.setData(lineData);
+                    stackedLineChart.invalidate();
+                    stackedLineChart.animateX(1000);
+
+                } else {
+                    Toast.makeText(getContext(), "Error: " + response.code(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<MonthlyTrendsModel>> call, Throwable t) {
+
+            }
+        });
+
     }
 
 }
